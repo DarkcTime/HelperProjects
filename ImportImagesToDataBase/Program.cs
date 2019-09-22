8b000ec197c246f270e3e3a6e5a61b71322e224d
Влад Чекмарёв <vlad_chekmarev_01@mail.ru>
2019-09-22 12:44:17 +0300

    Добавьте файлы проекта.

---
+++ ImportImagesToDataBase/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+
+namespace ImportImagesToDataBase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+        }
+
+        private Byte[] ConvertImageToByte(string path)
+        {
+            //создаем объект картинки
+            Bitmap image = new Bitmap(path);
+            //открываем поток для перевода картинки в байты 
+            using (MemoryStream ms = new MemoryStream())
+            {
+                //сохраняем картинку в поток 
+                image.Save(ms , image.RawFormat);
+                //возращаем массив байт картинки
+                return ms.ToArray(); 
+            }
+
+
+        }
+    }
+}
